0b4c025f5ea27cfb6519f92d204093570dd58eff
Vadym <vadim.mikhaylets@gmail.com>
2023-01-21 12:25:15 +0100

    Merge branch 'weather-api'

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Weather from './components/weatherTest/weather';
+
+
+function App() {
+  return (
+    <>
+      <Weather />
+    </>
+  );
+}
+
+export default App;
